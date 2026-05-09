894d51e52658ee865ecce3426fe361ceaa076c8b
eliesdrasdias <a0xlotus@gmail.com>
2026-05-09 19:18:18 -0300

    primeiro commit

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,25 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// A função main é a porta de entrada de um programa Go
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
+
+/*
+	Sobre comentários:
+
+	Trabalho real:
+	1) Priorizar código legível e fazer comentários que agrega valor.
+	2) Evitar comentários óbvios
+
+	Estudo:
+	3) Comentar bastante durante estudo
+*/
