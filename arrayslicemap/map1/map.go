6f2f1ecc547d48c7fefb491fc9c53f0a6283457e
eliesdrasdias <a0xlotus@gmail.com>
2026-05-14 09:19:23 -0300

    map1

---
+++ arrayslicemap/map1/map.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var aprovados map[int]string
+	// mapas devem ser inicializados
+	aprovados := make(map[int]string)
+
+	aprovados[12345678978] = "Maria"
+	aprovados[32448904082] = "Pedro"
+	aprovados[23929302393] = "Ana"
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("%s (CPF: %d)\n", nome, cpf)
+	}
+
+	fmt.Println(aprovados[23929302393])
+	delete(aprovados, 23929302393)
+	fmt.Println(aprovados[23929302393])
+}
