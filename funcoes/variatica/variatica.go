da5c6586e74d17eef9903479c9ac03ca33d112c7
Eliesdras Dias <a0xlotus@gmail.com>
2026-05-19 08:06:52 -0300

    funcoes variaticas

---
+++ funcoes/variatica/variatica.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func media(numeros ...float64) float64 {
+	total := 0.0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total / float64(len(numeros))
+}
+
+func main() {
+	fmt.Printf("Média: %.2f\n", media(7.7, 8.1, 5.9))
+}
